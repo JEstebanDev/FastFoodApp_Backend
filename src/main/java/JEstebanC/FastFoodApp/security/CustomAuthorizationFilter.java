3dd6751763b717322665f71ae26ed7a722fb241f
JEstebanC <castanoesteban9@gmail.com>
2022-04-07 16:46:13 -0500

    Adding the CORS

--- src/main/java/JEstebanC/FastFoodApp/security/CustomAuthorizationFilter.java
+++ src/main/java/JEstebanC/FastFoodApp/security/CustomAuthorizationFilter.java
@@ -34,13 +34,12 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
-
-	
 	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
+		
 		if(request.getServletPath().equals("/api/v1/login/") || request.getServletPath().equals("/api/v1/token-refresh/")) {
 			filterChain.doFilter(request, response);
 		}else {
