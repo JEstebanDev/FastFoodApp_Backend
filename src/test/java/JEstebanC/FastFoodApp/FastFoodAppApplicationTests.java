2446ca2293b9534ff2a8d60bd0a01eb4e3a41e75
JEstebanC <castanoesteban9@gmail.com>
2022-05-16 17:09:13 -0500

    deploy azure 2

--- src/test/java/JEstebanC/FastFoodApp/FastFoodAppApplicationTests.java
+++ src/test/java/JEstebanC/FastFoodApp/FastFoodAppApplicationTests.java
@@ -1,13 +1,13 @@
-package JEstebanC.FastFoodApp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FastFoodAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package JEstebanC.FastFoodApp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FastFoodAppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
