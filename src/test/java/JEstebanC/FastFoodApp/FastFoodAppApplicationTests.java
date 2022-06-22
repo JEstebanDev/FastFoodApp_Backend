5b55cf15d02d315adb15d268f98eb47b435e4fc1
JEstebanCDev <33663658+JEstebanCDev@users.noreply.github.com>
2022-06-22 10:57:54 -0500

    Update FastFoodAppApplicationTests.java

--- src/test/java/JEstebanC/FastFoodApp/FastFoodAppApplicationTests.java
+++ src/test/java/JEstebanC/FastFoodApp/FastFoodAppApplicationTests.java
@@ -1,4 +1,4 @@
-package JEstebanC.FastFoodApp;
+/**package JEstebanC.FastFoodApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class FastFoodAppApplicationTests {
 	}
 
 }
+**/
