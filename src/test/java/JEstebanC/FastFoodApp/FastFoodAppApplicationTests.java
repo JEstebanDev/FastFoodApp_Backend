327520d378b307fc39ea68ed84126f9159eec5fd
Duberly, Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2022-06-24 12:07:02 -0500

    Added more test. Added context tests when initializing Spring. Refactored ReportServiceImp.java and added mor info to the logs. Changed application.properties files to work on local environment as remote Azure connection has been temporally suspended, making Spring fail to initialize.

--- src/test/java/JEstebanC/FastFoodApp/FastFoodAppApplicationTests.java
+++ src/test/java/JEstebanC/FastFoodApp/FastFoodAppApplicationTests.java
@@ -1,16 +1,54 @@
 package JEstebanC.FastFoodApp;
 
+import JEstebanC.FastFoodApp.service.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FastFoodAppApplicationTests {
+    @Autowired
+    UserServiceImp userServiceImp;
+    @Autowired
+    CategoryServiceImp categoryServiceImp;
+    @Autowired
+    ProductServiceImp productServiceImp;
+    @Autowired
+    AdditionalServiceImp additionalServiceImp;
+    @Autowired
+    BillServiceImp billServiceImp;
+    @Autowired
+    CloudinaryService cloudinaryService;
+    @Autowired
+    CompanyServiceImp companyServiceImp;
+    @Autowired
+    OrdersServiceImp orderServiceImp;
+    @Autowired
+    PayModeServiceImp payModeServiceImp;
+    @Autowired
+    ReportServiceImp reportServiceImp;
+    @Autowired
+    SubscriberServiceImp subscriberServiceImp;
+    @Autowired
+    TaxServiceImp taxServiceImp;
 
-	@Test
-	void contextLoads() {
-        assertTrue(true);
-	}
+
+    @Test
+    void contextLoads() {
+        assertThat(userServiceImp).isNotNull();
+        assertThat(categoryServiceImp).isNotNull();
+        assertThat(productServiceImp).isNotNull();
+        assertThat(additionalServiceImp).isNotNull();
+        assertThat(billServiceImp).isNotNull();
+        assertThat(cloudinaryService).isNotNull();
+        assertThat(companyServiceImp).isNotNull();
+        assertThat(orderServiceImp).isNotNull();
+        assertThat(payModeServiceImp).isNotNull();
+        assertThat(reportServiceImp).isNotNull();
+        assertThat(subscriberServiceImp).isNotNull();
+        assertThat(taxServiceImp).isNotNull();
+    }
 
 }
